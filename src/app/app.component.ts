21fa9751bbdf3a4dcfcce1874870db609c626c68
xGuix <guillaumebrenot@gmail.com>
2024-03-02 16:19:41 +0100

    Update CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { AppModule } from './app.module';
 
 @Component({
+
   selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
   imports: [
     AppModule
   ],
-  standalone:true,
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  standalone: true,
 })
 export class AppComponent {
   title = 'Tour Of Heroes';
